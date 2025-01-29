a486f173acd3554c54c370ad32faf9f9c5928eb9
dudaanvsz <maria.nevesbussola@gmail.com>
2025-01-29 08:18:57 -0300

    Initial commit

---
+++ lista 1/lista.js
@@ -0,0 +1,53 @@
+//ex 1
+let a = 4
+let b = 3
+
+let soma = a + b 
+let sub = a - b 
+let mult = a * b
+let div = a / b
+let resto = a % b
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
+
+//ex 2
+
+let altura = 20
+let largura = 30
+
+let multi = largura * altura
+
+console.log(multi)
+
+// ex 3
+
+let num = 7
+
+let multti = 7 * 2
+let divi = 7 / 2
+
+console.log(multti)
+console.log(divi)
+
+// ex 4
+
+let nota1 = 9
+let nota2 = 8
+let nota3 = 7
+
+let somaa = nota1 + nota2 + nota3
+let divisao = soma / 3
+
+console.log(somaa)
+console.log(divisao)
+
+// ex 5
+
+let c = 27
+
+let 
+
