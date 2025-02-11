d1bd8b46dfaa02a3aab36c6c6974295f2801befb
dudaanvsz <maria.nevesbussola@gmail.com>
2025-02-11 13:08:16 -0300

    lista7

---
+++ Laço/lista7.js
@@ -0,0 +1,56 @@
+//ex 1
+let contador = 1
+while(contador <= 10) { 
+    console.log(contador) 
+     contador++ 
+}
+
+//ex2 
+let tabuada = prompt("Digite um número: ")
+let multiplicador = 1
+while (multiplicador <= 10) {
+    let resultado = tabuada * multiplicador
+     console.log(tabuada, "x", multiplicador, "=", resultado) 
+    multiplicador += 1 
+}
+
+//ex 3
+let controle = 10
+while(controle >= 1) { 
+    console.log(controle) 
+     controle-- 
+}
+
+//ex 4
+let senha = prompt("Digite a senha")
+while (senha  != 12345) {
+    senha = prompt("Tente novamente: ")
+    if (senha == 12345) {
+        console.log("Acertou")
+    }
+}
+
+//ex 5
+let contador2 = 50
+while(contador2 <= 100) { 
+    console.log(contador2) 
+     contador2++ 
+}
+
+//ex 6
+let controle2 = 0
+while(controle2 <= 100) { 
+    console.log(controle2) 
+     controle2 += 5
+}
+
+//ex 7
+let repeticao = 0
+while (repeticao < 5) {
+  console.log("Eu gosto de JavaScript")
+  repeticao++
+}
+
+//ex 8
+
+
