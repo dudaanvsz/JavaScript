094ff06c9e6e2e1ae1105a68be49aed314c91bf0
dudaanvsz <maria.nevesbussola@gmail.com>
2025-02-18 09:50:48 -0300

    atualização lista 9

---
+++ Laço/doWhile/for.js
@@ -0,0 +1,15 @@
+for(let contador = 0; contador <= 5; contador ++){
+console.log(contador)
+} //usando o for
+
+//
+for(let c = 5; c>= 0; c--) {
+    console.log(c)
+} //podemos fazer de forma regressiva
+
+//
+let numero = 4
+for(let mult = 1; mult <= 5; mult++) {
+ let resultado = mult * numero 
+ console.log(resultado)
+}
